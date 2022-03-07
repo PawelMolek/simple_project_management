393d25691a896720d036730d831baf50e276eaa0
PawelMolek <pawel.molek@gmail.com>
2022-03-07 20:02:01 +0100

    projects show page gain listing tasks and possibility to add task

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,3 +1,11 @@
 class Task < ApplicationRecord
   belongs_to :project
+
+  validates :status, inclusion: { in: ['not-started', 'in-progress', 'Complete'] }
+
+  STATUS_OPTIONS = [
+    ['Not started', 'not-started'],
+    ['In progress', 'in-progress'],
+    ['Complete', 'complete']
+  ]
 end
