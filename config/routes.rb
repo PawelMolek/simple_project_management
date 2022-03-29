96fe626c1cfc0bf42384b329e480b1ef27d844dd
PawelMolek <pawel.molek@gmail.com>
2022-03-29 10:52:17 +0200

    Footer included. Sign out button included.

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,8 @@ Rails.application.routes.draw do
     resources :tasks
   end
   devise_for :users
+  devise_scope :user do
+    get '/users/sign_out' => 'devise/sessions#destroy'
+  end
   root "projects#index"
 end
