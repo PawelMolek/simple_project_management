0006e2ffab47796a421b5855e806a2b3a91b1087
PawelMolek <pawel.molek@gmail.com>
2022-03-30 12:22:40 +0200

    Devise mailer initially configured

--- Gemfile
+++ Gemfile
@@ -13,6 +13,8 @@ gem "tzinfo-data", platforms: %i[ mingw mswin x64_mingw jruby ]
 gem "bootsnap", require: false
 gem 'devise', '~> 4.8', '>= 4.8.1'
 gem 'nested_scaffold', '~> 1.1'
+gem "image_processing", ">= 1.2"
+gem "figaro"
 
 group :development, :test do
   gem "debug", platforms: %i[ mri mingw x64_mingw ]
--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -60,13 +60,13 @@ class ProjectsController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_project
-      @project = current_user.projects.find(params[:id])
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_project
+    @project = current_user.projects.find(params[:id])
+  end
 
-    # Only allow a list of trusted parameters through.
-    def project_params
-      params.require(:project).permit(:name, :description)
-    end
+  # Only allow a list of trusted parameters through.
+  def project_params
+    params.require(:project).permit(:name, :description)
+  end
 end
