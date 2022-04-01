6f99ea820d8ab31bea9ff8aa7ab12248b547aaec
PawelMolek <pawel.molek@gmail.com>
2022-04-01 23:01:35 +0200

    authentication

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,5 +1,4 @@
 class ProjectsController < ApplicationController
-  before_action :authenticate_user!
   before_action :set_project, only: %i[ show edit update destroy ]
 
   # GET /projects or /projects.json
@@ -60,9 +59,12 @@ class ProjectsController < ApplicationController
   end
 
   private
+
   # Use callbacks to share common setup or constraints between actions.
   def set_project
+
     @project = current_user.projects.find(params[:id])
+
   end
 
   # Only allow a list of trusted parameters through.
