7d2a42a07d1cf88e22aeefea88de04fe631f752d
PawelMolek <pawel.molek@gmail.com>
2022-04-03 20:34:29 +0200

    project delete works correctly

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -52,10 +52,7 @@ class ProjectsController < ApplicationController
   def destroy
     @project.destroy
 
-    respond_to do |format|
-      format.html { redirect_to projects_url, notice: "Project was successfully destroyed." }
-      format.json { head :no_content }
-    end
+    redirect_to projects_url, notice: 'Project was successfully destroyed.', status: :see_other
   end
 
   private
